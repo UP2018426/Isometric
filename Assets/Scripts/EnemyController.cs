4492a942a4afbb26e7b3abad4988aa7789452aa2
UP2018426 <isufranc@gmail.com>
2025-12-19 00:49:45 +0000

    Added enemy following

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,19 +1,33 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
     public int health;
+    [SerializeField] private float closestDistance;
+    private NavMeshAgent agent;
+
+    private Transform player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = player.position;
+        if ((transform.position - player.transform.position).magnitude > closestDistance)
+        {
+            agent.isStopped = false;
+        }
+        else
+        {
+            agent.isStopped = true;
+        }
     }
 
     public void TakeDamage(int damageTaken)
@@ -23,7 +37,7 @@ public class EnemyController : MonoBehaviour
         if(health <= 0)
         {
             Debug.Log("Enemy is dead");
-            Destroy(this.gameObject, 1f);
+            Destroy(this.gameObject, 0.2f);
         }
     }
 }
